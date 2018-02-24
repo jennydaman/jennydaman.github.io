04f6e0fb6c07b89826b984b9de8aeef46933d6a5
Jennings Zhang <jenni_zh@protonmail.com>
2018-02-24 18:25:10 -0500

    Full resume

--- resume/mkresume.sh
+++ resume/mkresume.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -x
 
+xelatex -interaction nonstopmode full > /dev/null 2> /dev/null
+mv full.pdf full/jzhang_resume.pdf
+
+
 xelatex -interaction nonstopmode brief > /dev/null 2> /dev/null
 mv brief.pdf brief/jzhang_resume.pdf
+
 
 for f in $(cat .gitignore) ; do 
   rm "$f"
 done
+exit 0
