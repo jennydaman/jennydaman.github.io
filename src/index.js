2112a0eb4b5ae28f489a0cb9d14c1a1a231e315d
Jennings Zhang <jenni_zh@protonmail.com>
2018-03-03 18:06:11 -0500

    Clean up again

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import Side from './components/side';
 import Resume from './components/resume';
 
 import Fab from 'preact-material-components/Fab';
 import 'preact-material-components/Fab/style.css';
 import 'preact-material-components/Icon/style.css';
-import style from './style.css'
+import './style.css';
 
 if (module.hot) {
   require('preact/debug');
 }
 
+const App = () => (
+  <div>
+    <div id="app">
+      <Side />
+      <Resume />
+    </div>
+    <div id="corner">
+      <span id="tooltip">Email Me</span>
+      <Fab ripple><a href="mailto:jenni_zh@protonmail.com">
+        <img src="/assets/ic_email_white_24px.svg" />
+      </a></Fab>
+    </div>
+  </div>
+);
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <div id="app">
-          <Side />
-          <Resume />
-        </div>
-        <div id="corner">
-          <span id="tooltip">Email Me</span>
-          <Fab ripple={true}><a href="mailto:jenni_zh@protonmail.com">
-            <img src="/assets/ic_email_white_24px.svg" />
-          </a></Fab>
-        </div>
-      </div>
-
-    );
-  }
-}
+export default App;
