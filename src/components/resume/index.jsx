96ec367ac2b89c539e3cc829f4c012ffc3cfcd5a
Jennings Zhang <jenni_zh@protonmail.com>
2018-03-03 14:12:34 -0500

    Email FAB

--- src/components/resume/index.jsx
+++ src/components/resume/index.jsx
@@ -16,37 +16,45 @@ class Tab extends Component {
   }
 }
 
+const Pdf = props => {
+  let resPath = '/assets/resume/' + props.fname;
+  return (
+    <main>
+      <p><a href={resPath}>Direct link to {props.fname}</a></p>
+      <embed src={resPath} type="application/pdf" height="600" width="100%" />
+    </main>
+  )
+}
+
 
 export default class Resume extends Component {
-
-  state = { fname: 'jzhang_resume.pdf' }
 
   changeFile = fname => {
     this.setState({ fname: fname });
   }
 
-  render() {
+  componentDidMount() {
+    this.changeFile('jzhang_resume.pdf');
+  }
 
-    let resPath = '/assets/resume/' + this.state.fname;
+  render(props, state) {
     return (
-      <main>
+      <div>
         <div class={style.toolbar}>
           <h1>Résumé</h1>
 
           <div class={style.last}>
             <Tabs indicator-accent={true}>
-              <Tab fname="jzhang_resume.pdf" onTabClick={this.changeFile} active={true} class={style.full}>Full</Tab>
+              <Tab fname="jzhang_resume.pdf" onTabClick={this.changeFile} class={style.full}>Full</Tab>
               <Tab fname="jzhang_brief.pdf" onTabClick={this.changeFile} class={style.brief}>Brief</Tab>
             </Tabs>
           </div>
         </div>
 
         <p>Last updated 01 March 2018</p>
-
         <div class={style.divider}></div>
-        <p><a href={resPath}>Direct link to {this.state.fname}</a></p>
-        <embed src={resPath} type="application/pdf" height="600" width="100%" />
-      </main>
+        <Pdf fname={state.fname} />
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import style from './style.css'
 import { h, Component } from 'preact';
 import Side from './components/side';
 import Resume from './components/resume';
+
+import Fab from 'preact-material-components/Fab';
+import 'preact-material-components/Fab/style.css';
+import 'preact-material-components/Icon/style.css';
+import style from './style.css'
+
 if (module.hot) {
   require('preact/debug');
 }
@@ -11,10 +16,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <div id="app">
-        <Side />
-        <Resume />
+      <div>
+        <div id="app">
+          <Side />
+          <Resume />
+        </div>
+        <div id="corner">
+          <Fab ripple={true}><a href="mailto:jenni_zh@protonmail.com">
+            <Fab.Icon>email</Fab.Icon>
+          </a></Fab>
+        </div>
       </div>
+
     );
   }
 }
